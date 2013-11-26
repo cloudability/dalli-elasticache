f95fa827301adebcf0c03bc5d5de9d709a90b7e1
Brad Heller <brad@cloudability.com>
2013-11-26 14:24:52 -0800

    Add support for connections to non-elasticache instances to make development workflow easier.

--- lib/dalli/elasticache.rb
+++ lib/dalli/elasticache.rb
@@ -30,6 +30,7 @@ module Dalli
       s.puts "config get cluster\r\n"
       data = []
       while (line = s.gets) != "END\r\n"
+        break if data == "ERROR\r\n"
         data << line
       end
 
@@ -40,10 +41,22 @@ module Dalli
     def data
       return @data if @data
       raw_data = config_get_cluster
-      version = raw_data[1].to_i
-      instance_data = raw_data[2].split(/\s+/)
-      instances = instance_data.map{ |raw| host, ip, port = raw.split('|'); {:host => host, :ip => ip, :port => port} }
-      @data = { :version => version, :instances => instances }
+
+      # We didn't get raw data back so we're assuming it's not in clustered
+      # mode. We'll synthesize our own data out of that.
+      if raw_data.empty?
+        raw_stats = stats
+
+        instance = { :host => config_host, :ip => resolved_config_host, :port => config_port }
+
+        # TODO: Version should be...anything else.
+        @data = { :version => 1, :instances => [instance] }
+      else
+        version = raw_data[1].to_i
+        instance_data = raw_data[2].split(/\s+/)
+        instances = instance_data.map{ |raw| host, ip, port = raw.split('|'); {:host => host, :ip => ip, :port => port} }
+        @data = { :version => version, :instances => instances }
+      end
     end
 
     def version
@@ -54,5 +67,19 @@ module Dalli
       data[:instances].map{ |i| "#{i[:ip]}:#{i[:port]}" }
     end
 
+    def resolved_config_host
+      # If it's already an IP address, let's not worry 'bout it.
+      return config_host.strip if config_host.strip =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/
+
+      resolv = Resolv::DNS.new
+      addresses = resolv.getaddresses(config_host)
+      ipv4 = addresses.detect { |addr| addr.is_a?(Resolv::IPv4) }
+
+      if ipv4.nil?
+        raise RuntimeError, "Failed to resolve #{config_host}"
+      end
+
+      ipv4.to_s
+    end
   end
 end
